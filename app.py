4416204e6cc3617544cfd3f237db011bc7233b89
andres aarnela <47285499+source-exp@users.noreply.github.com>
2026-04-14 13:20:12 +0300

    Add user profile, fix code structure

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import abort, render_template, request, session, redirect
-from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 import config
 import db
 import items
 import re
+import users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -18,6 +18,14 @@ def require_login():
 def index():
     all_items = items.get_items()
     return render_template("index.html", items=all_items)
+
+@app.route("/user/<int:user_id>")
+def show_user(user_id):
+    user = users.get_user(user_id)
+    if not user:
+        abort(404)
+    items = users.get_items(user_id)
+    return render_template("show_user.html", user=user, items=items)
 
 @app.route("/new_item")
 def new_item():
@@ -118,11 +126,9 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return render_template("register.html", error="VIRHE: salasanat eivät ole samat")
-    password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return render_template("register.html", error="VIRHE: tunnus on jo varattu")
 
@@ -137,15 +143,9 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        rows = db.query(sql, [username])
-        if not rows:
-            return render_template("login.html", error="Väärä tunnus tai salasana")
-        result = rows[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
+        user_id = users.check_login(username,password)
 
-        if check_password_hash(password_hash, password):
+        if user_id:        
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
